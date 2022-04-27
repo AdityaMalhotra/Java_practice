ddf93e8313801c0a8c6272e696530e383902e4a7
AdityaMalhotra <adityamalhotra1995@gmail.com>
2022-04-27 22:50:13 +0530

    Added more documentation for few problems

--- AbsoluteDifference.java
+++ AbsoluteDifference.java
@@ -6,6 +6,10 @@ public class AbsoluteDifference
         int arr[] = {-59,-36,-13,1,-53,-92,-2,-96,-54,75};
         System.out.println(minimumAbsoluteDifference(arr));
     }
+
+    /**
+     * n(log(n)) Solution to https://www.hackerrank.com/challenges/minimum-absolute-difference-in-an-array/problem
+     */
     // Complete the minimumAbsoluteDifference function below.
     static int minimumAbsoluteDifference(int[] arr) {
         Arrays.sort(arr);
--- AlternatingCharacters.java
+++ AlternatingCharacters.java
@@ -3,6 +3,9 @@ public class AlternatingCharacters
     public static void main(String[] args) {
         System.out.println(alternatingCharacters("AAABABA"));
     }
+    /*
+        O(n) solution to https://www.hackerrank.com/challenges/alternating-characters/problem
+     */
     // Complete the alternatingCharacters function below.
     static int alternatingCharacters(String s) {
         int count = 0;
